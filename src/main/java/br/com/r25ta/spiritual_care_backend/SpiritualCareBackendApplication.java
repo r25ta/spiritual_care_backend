64724855774a3304f19566ed0a02bf052e692389
r25ta <twentyfive@hotmail.com>
2023-06-18 15:29:52 -0300

    First commit

---
+++ src/main/java/br/com/r25ta/spiritual_care_backend/SpiritualCareBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.r25ta.spiritual_care_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiritualCareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiritualCareBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/r25ta/spiritual_care_backend/SpiritualCareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.r25ta.spiritual_care_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiritualCareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
